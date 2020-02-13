a25d7883ad3ebff2675c899293cb9d7942c36f4c
yigitismail <smlygt@icloud.com>
2020-02-13 21:02:55 +0300

    design

---
+++ app/src/main/java/com/yigitismail/landmarkbook/DetailActivity.java
@@ -0,0 +1,23 @@
+package com.yigitismail.landmarkbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView landmarkNameText = findViewById(R.id.landmarkNameText);
+        TextView countryNameText = findViewById(R.id.countryNameText);
+
+
+
+    }
+}
