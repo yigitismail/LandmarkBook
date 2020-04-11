125bc4d74224475c3c67b91ea09cb63121a4ddda
yigitismail <smlygt@icloud.com>
2020-04-11 17:21:04 +0300

    f

--- app/src/main/java/com/yigitismail/landmarkbook/MainActivity.java
+++ app/src/main/java/com/yigitismail/landmarkbook/MainActivity.java
@@ -2,6 +2,8 @@ package com.yigitismail.landmarkbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -17,16 +19,45 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.listView);
 
-        ArrayList<String> landmarkNames = new ArrayList<String>();
+        // Data
+
+        ArrayList<String> landmarkNames = new ArrayList<>();
 
         landmarkNames.add("Pisa");
         landmarkNames.add("Eiffel");
-        landmarkNames.add("Collesium");
+        landmarkNames.add("Coloseo");
         landmarkNames.add("London Bridge");
+
+        ArrayList<String> countryNames  = new ArrayList<>();
+
+        countryNames.add("Italy");
+        countryNames.add("France");
+        countryNames.add("Italy");
+        countryNames.add("United Kingdom");
+
+        Bitmap pisa = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.pisa);
+        Bitmap eiffel = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.eiffel);
+        Bitmap coloseo = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.coloseo);
+        Bitmap londonBridge = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.londonbridge);
+
+        ArrayList<Bitmap> landmarkImages = new ArrayList<>();
+
+        landmarkImages.add(pisa);
+        landmarkImages.add(eiffel);
+        landmarkImages.add(coloseo);
+        landmarkImages.add(londonBridge);
+
+
+
+
+
+        // ListView
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_expandable_list_item_1,landmarkNames);
 
         listView.setAdapter(arrayAdapter);
+
+
 
 
 
